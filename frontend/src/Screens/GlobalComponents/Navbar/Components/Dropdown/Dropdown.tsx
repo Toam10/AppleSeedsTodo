5cb33e5a7f95b0b44cd9e576224f151dc181b7d2
Adam Birnbaum <adambiren19@gmail.com>
2022-02-28 13:23:36 +0200

    Adding navbar basic design and dropdown list

---
+++ frontend/src/Screens/GlobalComponents/Navbar/Components/Dropdown/Dropdown.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Dropdown.css';
+
+function Dropdown() {
+    return (
+        <h2 className='dropdown'>Select Project &nbsp;&#9654;</h2>
+    );
+};
+
+export default Dropdown;
---
+++ frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dropdown from './Components/Dropdown/Dropdown';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <Dropdown />
+            <h2 className='about'>About</h2>
+            <h2 className='logo'>LOGO</h2>
+        </div>
+    );
+};
+
+export default Navbar;
