7a405c7721d418b4e807712dcd08da8efa0b57b2
Eli <elikampel@yahoo.com>
2022-03-01 10:42:33 +0200

    fixing 59-62

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import { app } from './app';
+import {connect} from "mongoose"
+
+const URI = `mongodb://localhost:27017/AppleSeedsTodo`
+connect(URI) 
 
 const PORT = 8080;
 const SERVER_MESSAGE = `server is up and running on port ${PORT}`;
 
 // tslint:disable-next-line: no-console
 app.listen(PORT, () => console.log(SERVER_MESSAGE));
+
