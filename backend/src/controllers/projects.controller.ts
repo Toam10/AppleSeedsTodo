045333168af2d7b1a08821a87f6f762d41d724ad
salitordjman <salitorgi@gmail.com>
2022-03-01 10:28:04 +0200

    fix issues

--- backend/src/controllers/projects.controller.ts
+++ backend/src/controllers/projects.controller.ts
@@ -3,19 +3,19 @@ import { Project, projects } from '../fakeData/project.fakeData';
 
 async function getProjectDetails(req: Request, res: Response) {
   try {
-    const projectDetail = await projects.filter((project) => project.id === req.params.id);
+    const projectDetail = projects.find((project) => project.id === req.params.id);
 
     // const projectDetail =await projects.findById(req.params.id);
 
-    if (projectDetail.length === 0) return res.status(404).send('Not found a Project');
+    if (!projectDetail) throw new Error('Not found a Project');
 
     res.status(200).send(projectDetail);
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send((error as Error).message);
   }
 }
 
-function getAllProjectDetails(req: Request, res: Response) {
+async function getAllProjectDetails(req: Request, res: Response) {
   try {
     const projectFilter: Project[] = projects.map((project): Project => {
       // const { name, description, goal } = project;
@@ -30,7 +30,7 @@ function getAllProjectDetails(req: Request, res: Response) {
 
 async function createProject(req: Request, res: Response) {
   try {
-    const { name, description, goal, status: boolean } = req.body;
+    const { name, description, goal, status } = req.body;
 
     // const newProject = new projects({ name, description, goal, status })
     // const reciveNewProject = await newProject.save();
