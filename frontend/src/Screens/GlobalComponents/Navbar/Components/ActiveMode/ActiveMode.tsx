a011dfcca5c16cecbe48338bf0fcb49b01edd9d9
Adam Birnbaum <adambiren19@gmail.com>
2022-03-01 09:32:27 +0200

    Default mode update

---
+++ frontend/src/Screens/GlobalComponents/Navbar/Components/ActiveMode/ActiveMode.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ActiveMode() {
+    return (
+        <div>
+            <h2 className='dropdown'>Select Project &nbsp;&#11206;</h2>
+        </div>
+    )
+};
+
+export default ActiveMode;
--- frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
+++ frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ActiveMode from './Components/ActiveMode/ActiveMode';
 import Dropdown from './Components/Dropdown/Dropdown';
 import './Navbar.css';
 
 function Navbar() {
     return (
         <div className='navbar'>
-            <Dropdown />
+            <ActiveMode />
             <h2 className='about'>About</h2>
             <h2 className='logo'>LOGO</h2>
         </div>
