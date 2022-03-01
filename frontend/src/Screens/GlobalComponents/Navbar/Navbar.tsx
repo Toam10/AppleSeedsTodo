15ad80eae0dc7d3ee03aa0059d970be1f5109705
Adam Birnbaum <adambiren19@gmail.com>
2022-03-01 10:52:34 +0200

    Navbar Basic design cleanup

--- frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
+++ frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import Dropdown from './Components/Dropdown/Dropdown';
+import DefaultMode from './Components/Dropdown/DefaultMode';
 import './Navbar.css';
+import '../../../Style/Colors.css'
 
 function Navbar() {
     return (
         <div className='navbar'>
-            <Dropdown />
-            <h2 className='about'>About</h2>
-            <h2 className='logo'>LOGO</h2>
+            <div className='leftSide'>
+                <DefaultMode />
+                <h2 className='about'>About</h2>
+            </div>
+            <div className='rightSide'>
+                <h2 className='logo'>LOGO</h2>
+            </div>
         </div>
     );
 };
