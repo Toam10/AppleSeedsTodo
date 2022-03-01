895736647ad781a131952e1f7529a4adbae108f7
Eli <elikampel@yahoo.com>
2022-03-01 16:06:26 +0200

    added mongoose and ts@

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import {connect} from "mongoose"
 
 const URI = `mongodb://localhost:27017/AppleSeedsTodo`
-connect(URI) 
+connect(URI)
 
 const PORT = 8080;
 const SERVER_MESSAGE = `server is up and running on port ${PORT}`;
