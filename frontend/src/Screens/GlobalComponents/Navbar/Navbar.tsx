86a1be1e62e73c00c988ce9af2fa977728e62971
Adam Birnbaum <adambiren19@gmail.com>
2022-03-01 11:26:09 +0200

    Navbar default mode cleanup and update

--- frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
+++ frontend/src/Screens/GlobalComponents/Navbar/Navbar.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import ActiveMode from './Components/ActiveMode/ActiveMode';
-import Dropdown from './Components/Dropdown/Dropdown';
-=======
 import DefaultMode from './Components/Dropdown/DefaultMode';
->>>>>>> tg2-35_navbar_basic-design
 import './Navbar.css';
 import '../../../Style/Colors.css'
 
 function Navbar() {
     return (
         <div className='navbar'>
-<<<<<<< HEAD
-            <ActiveMode />
-            <h2 className='about'>About</h2>
-            <h2 className='logo'>LOGO</h2>
-=======
             <div className='leftSide'>
                 <DefaultMode />
                 <h2 className='about'>About</h2>
@@ -23,7 +13,6 @@ function Navbar() {
             <div className='rightSide'>
                 <h2 className='logo'>LOGO</h2>
             </div>
->>>>>>> tg2-35_navbar_basic-design
         </div>
     );
 };
