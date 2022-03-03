3b0b19f7870b38291b8feed3716b5e40e1464cd5
Adam Birnbaum <adambiren19@gmail.com>
2022-03-03 23:53:43 +0200

    Project card design and page layout

--- frontend/src/Screens/GlobalComponents/Navbar/Components/PageDesign/ProjectCards/ProjectCards.tsx
+++ frontend/src/Screens/GlobalComponents/Navbar/Components/PageDesign/ProjectCards/ProjectCards.tsx
@@ -6,7 +6,7 @@ function ProjectCards() {
     return (
         <div className="Cards__container">
             <ul className="Cards__Card">
-                <li>Project 1</li>
+                <li>Project</li>
                 <li><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. et dolore magna aliqua.</li>
                 <li><strong>Goal:</strong> Lorem ipsum dolor sit.</li>
             </ul>
