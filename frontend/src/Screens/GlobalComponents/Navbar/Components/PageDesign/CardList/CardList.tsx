72926b79c91761e410ab1b21b25b1b391cf68b49
Adam Birnbaum <adambiren19@gmail.com>
2022-03-06 13:00:47 +0200

    Building and implementation of project card

--- frontend/src/Screens/GlobalComponents/Navbar/Components/PageDesign/CardList/CardList.tsx
+++ frontend/src/Screens/GlobalComponents/Navbar/Components/PageDesign/CardList/CardList.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './CardList.css';
-
+import ProjectCard from "./ProjectCard/ProjectCard";
 
 function CardList() {
     return (
-        <div className="CardsList__container">
-            <ul className="Cards__Card">
-                <li>Project</li>
-                <li><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. et dolore magna aliqua.</li>
-                <li><strong>Goal:</strong> Lorem ipsum dolor sit.</li>
-            </ul>
+        <div className="CardsList">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+
         </div>
     );
 };
