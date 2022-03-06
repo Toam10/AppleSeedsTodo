83de72630a171fd6324f9f9e18de3fd00f9bb75e
salitordjman <salitorgi@gmail.com>
2022-03-06 12:30:17 +0200

    TG2_54_board_page_board_card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css';
 import PageDesign from './Screens/GlobalComponents/Navbar/Components/PageDesign/PageDesign';
 import './Screens/GlobalComponents/Navbar/Navbar';
 import Navbar from './Screens/GlobalComponents/Navbar/Navbar';
+import PageBoard from './Screens/GlobalComponents/Navbar/Components/PageBoard/PageBoard';
 
 function App() {
   return (
     <div className='App'>
+      <Router>
       <Navbar />
-      <PageDesign />
+      
+      <Routes>
+
+      <Route path="/" element={<PageDesign />} />
+      <Route path="board/:idProject" element={<PageBoard />} />
+
+
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/Screens/GlobalComponents/Navbar/Components/PageBoard/PageBoard.tsx
@@ -0,0 +1,13 @@
+import './PageBoard.css';
+
+function PageBoard() {
+  return (
+      <div>
+          <div>
+              <h2>Board</h2>
+          </div>
+      </div>
+  );
+};
+
+export default PageBoard;
