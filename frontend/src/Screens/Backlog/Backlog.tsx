8258eb9dccaa8e3b5428b8d84f4eec44903f72bc
Eli <elikampel@yahoo.com>
2022-03-07 03:03:19 +0200

    bcklogstatic with tasks

--- frontend/src/Screens/Backlog/Backlog.tsx
+++ frontend/src/Screens/Backlog/Backlog.tsx
@@ -1,38 +1,47 @@
 /**
  *  <Link to={'/addTaskBacklog/'} className="button">Add Task</Link><Link to={'/board/'} className="button">Move To Board</Link>
  */
-// import './Backlog.css';
-import Task from './components/Task/Task';
+import './Backlog.css';
+import '../../Style/Colors.css'
+// import Task from './components/Task/Task';
+// import { ITask } from '../../interfaces';
 // import { Link } from 'react-router-dom';
-import { projects } from './fakeData/project.fakeData';
-import {IProject} from '../../interfaces';
-const { name, tasks } = projects[0];
-Backlog();
-
+// import {IProject} from '../../interfaces';
 function Backlog() {
-    // const props = projects[0];
-    // const { name, tasks } = props;
-
     return (
         <div className='backlog'>
             <div className='backlog__header'>
-                <button className='projectName'>{name}</button>
+                <label className='backlog__label'>Project Name</label>
             </div>
             <div className='backlog__twoButtons'>
-                <button className='projectName'>Add Task</button>
-                <button className='projectName'>Move To Board</button>
+                <button className='backlog__btn1'>Add Task</button>
+                <button className='backlog__btn2'>Move To Board</button>
             </div>
             <div className='backlog__table'>
                 <table>
                     <thead>
-                        <tr>
-                            <th>Task Name</th>
-                            <th>Task Date</th>
-                            <th>Task Urgency</th>
+                        <tr className='backlog__table_header'>
+                            <th className='taskName'>Task Name</th>
+                            <label className='line1'>|</label>
+                            <th className='taskDate'>Task Date</th>
+                            <label className='line2'>|</label>
+                            <th className='taskUrgency'>Task Urgency</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {}
+
+                        <tr className='task'>
+                            <td className='task1_1'>to do</td>
+                            <td className='task1_2'>20/20/22</td>
+                            <td className='task1_3'>2</td>
+                        </tr>
+                        <tr className='task'>
+                            <td className='task1_1'>to do2</td>
+                            <td className='task1_2'>20/20/222</td>
+                            <td className='task1_3'>22</td>
+                        </tr>
+
+
                     </tbody>
                 </table>
             </div>
@@ -41,3 +50,14 @@ function Backlog() {
 };
 
 export default Backlog;
+
+/**
+ * 
+ *  {
+                            currentProject.map(task => (
+                                <tr key={task.id}>
+                                    <Task task={task} />
+                                </tr>
+                            ))
+                        }
+ */
